67f86f6e6198ef8bd94b14fc9337699b61b21233
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2022-11-17 16:24:55 +0100

    added structure of add

---
+++ src/list/HTLStack.java
@@ -0,0 +1,7 @@
+package list;
+
+public interface HTLStack {
+    public void push(int value);
+    public void peak(int index);
+    public void pop(int index);
+}
---
+++ src/list/stack/Stack.java
@@ -0,0 +1,19 @@
+package list.stack;
+
+import list.HTLStack;
+
+public class Stack implements HTLStack {
+    private Node root;
+
+    @Override
+    public void push(int value) {
+    }
+
+    @Override
+    public void peak(int index) {
+    }
+
+    @Override
+    public void pop(int index) {
+    }
+}
